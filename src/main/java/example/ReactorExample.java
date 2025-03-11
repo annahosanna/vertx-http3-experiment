cb3c64916895d407bba5f326aa9d9d607a3ae46e
annahosanna <annahosanna@gmail.com>
2025-03-10 22:51:24 -0400

    Something crazy is going on. I changed the word reactor to projectreactor and back again and eclipe was happy

--- src/main/java/example/ReactorExample.java
+++ src/main/java/example/ReactorExample.java
@@ -1,7 +1,7 @@
 package example;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Mono; 
 
 public class ReactorExample {
     public static void main(String[] args) {

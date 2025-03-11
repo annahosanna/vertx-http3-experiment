1a89201db7f5a055ab1adfe0cafb28a0bcde1db5
annahosanna <annahosanna@gmail.com>
2025-03-11 16:53:57 -0400

    Added logic for self signed cert. Fixed missing imports etc.

--- src/main/java/example/MainServer.java
+++ src/main/java/example/MainServer.java
@@ -2,35 +2,35 @@ package example;
 
 import java.security.cert.CertificateException;
 
-import io.netty.handler.ssl.ApplicationProtocolConfig;
-import io.netty.handler.ssl.ApplicationProtocolNames;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
+// import io.netty.handler.ssl.ApplicationProtocolConfig;
+// import io.netty.handler.ssl.ApplicationProtocolNames;
+// import io.netty.handler.ssl.SslContextBuilder;
+// import io.netty.handler.ssl.SslContext;
+// import io.netty.handler.ssl.util.SelfSignedCertificate;
 // Required Imports
-import io.netty.incubator.codec.http3.Http3ConnectionHandler;
-import io.netty.incubator.codec.http3.Http3HeadersFrame;
+// import io.netty.incubator.codec.http3.Http3ConnectionHandler;
+// import io.netty.incubator.codec.http3.Http3HeadersFrame;
 import io.netty.incubator.codec.quic.QuicSslContext;
 import io.netty.incubator.codec.quic.QuicSslContextBuilder;
-import io.netty.incubator.codec.http3.Http3ServerConnectionHandler;
-import reactor.core.publisher.Mono;
+// import io.netty.incubator.codec.http3.Http3ServerConnectionHandler;
+// import reactor.core.publisher.Mono;
 import reactor.netty.incubator.quic.QuicServer;
-import io.netty.incubator.codec.quic.QuicServerCodecBuilder;
+// import io.netty.incubator.codec.quic.QuicServerCodecBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 class MainServer {
 
   // public Mono<Void> start() {
 	public static void main(String[] args) {
-	  SelfSignedCertificate ssc = null;
+		SelfSignedCertGenerator ssc = null;
 	  try {
-		ssc = new SelfSignedCertificate();
+		ssc = new SelfSignedCertGenerator();
 	} catch (CertificateException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
       QuicSslContext context = QuicSslContextBuilder
-    		  .forServer(ssc.key(), "", ssc.cert())
+    		  .forServer(ssc.getPrivateKey(), null, ssc.getCertificate())
     		  	// .forClient()
     		    .trustManager(InsecureTrustManagerFactory.INSTANCE)
     		    .applicationProtocols("h3") // ALPN protocol for HTTP/3
---
+++ src/main/java/example/SelfSignedCertGenerator.java
@@ -0,0 +1,78 @@
+package example;
+
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+
+public class SelfSignedCertGenerator {
+
+  private KeyPair keyPair;
+  private X509Certificate certificate;
+
+  public SelfSignedCertGenerator() throws Exception {
+    generateKeyPair();
+    generateCertificate();
+  }
+
+  private void generateKeyPair() throws NoSuchAlgorithmException {
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    keyPairGenerator.initialize(2048);
+    this.keyPair = keyPairGenerator.generateKeyPair();
+  }
+
+  private void generateCertificate() throws Exception {
+    X500Name issuerName = new X500Name("CN=localhost");
+    X500Name subjectName = new X500Name("CN=localhost");
+
+    BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());
+    Date startDate = new Date(System.currentTimeMillis());
+    Date endDate = new Date(
+      System.currentTimeMillis() + 365 * 24 * 60 * 60 * 1000L
+    );
+
+    X509v3CertificateBuilder certBuilder = new X509v3CertificateBuilder(
+      issuerName,
+      serialNumber,
+      startDate,
+      endDate,
+      subjectName,
+      SubjectPublicKeyInfo.getInstance(this.keyPair.getPublic().getEncoded())
+    );
+
+    // Add Subject Alternative Name
+    GeneralName altName = new GeneralName(GeneralName.dNSName, "localhost");
+    GeneralNames subjectAltName = new GeneralNames(altName);
+    certBuilder.addExtension(
+      Extension.subjectAlternativeName,
+      false,
+      subjectAltName
+    );
+
+    ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSA").build(
+      this.keyPair.getPrivate()
+    );
+    X509CertificateHolder certHolder = certBuilder.build(signer);
+
+    this.certificate = new JcaX509CertificateConverter()
+      .getCertificate(certHolder);
+  }
+
+  public PrivateKey getPrivateKey() {
+    return this.keyPair.getPrivate();
+  }
+
+  public X509Certificate getCertificate() {
+    return certificate;
+  }
+}
